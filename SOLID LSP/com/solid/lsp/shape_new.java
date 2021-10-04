c66b21154a5f9369eee534648680b3a2a0867174
Nuttapong <75092343+nuttapong42@users.noreply.github.com>
2021-10-05 00:06:50 +0700

    Add files via upload

---
+++ SOLID LSP/com/solid/lsp/shape_new.java
@@ -0,0 +1,41 @@
+package com.solid.lsp;
+
+public abstacct class Shape {
+
+    private int width;
+    private int height;
+
+    Shape_new() {
+        Shape (int width, int height){
+            this.width = width;
+            this.height = height;
+            
+        }
+
+        public int getHeight(){
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height;
+        }
+
+        public int getWidth(){
+            return width;
+        }
+
+        public void setWidth(int width) {
+            return width;
+        }
+
+        public int getArea() {
+            return width*height;
+        }
+
+
+
+
+
+    }
+
+}
