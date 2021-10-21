621662e7639f0187a642ce9adc8671dc84b468b8
Nuttapong <75092343+nuttapong42@users.noreply.github.com>
2021-10-21 18:26:52 +0700

    Add files via upload
    Commit Lab08 nuttapong

---
+++ LAB8/SoftArchDesign/src/edu/parinya/softarchdesign/structural/HealthcareWorkerDecorator.java
@@ -0,0 +1,22 @@
+package edu.parinya.softarchdesign.structural;
+
+public abstract class HealthcareWorkerDecorator extends HealthcareWorker {
+    protected HealthcareWorker worker;
+
+    public HealthcareWorkerDecorator(HealthcareWorker worker) {
+        super(worker);
+        this.worker = worker;
+
+    }
+
+
+    public void service() {
+        worker.service();
+
+    }
+
+    public double getPrice() {
+
+        return worker.getPrice();
+    }
+}
